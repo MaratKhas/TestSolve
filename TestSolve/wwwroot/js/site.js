0c4fa63c7f219d6f18be95b5c30146a470439543
MaratKhas <marat.has123@mail.ru>
2022-04-13 23:43:05 +0300

    добавлена валидация формы ввода количества студентов

--- TestSolve/wwwroot/js/site.js
+++ TestSolve/wwwroot/js/site.js
@@ -1,1 +1,18 @@
-﻿
+﻿$(document).ready(function () {
+    $("#StudentCountForm").validate({
+        rules: {
+            CountStudent: {
+                required: true,
+                range: [2,100]
+            },
+            
+        },
+        messages: {
+            CountStudent: {
+                required: "Это поле обязательно для заполнения",
+                range : "Количество студентов должно быть от 2 до 100"
+            },
+           
+        }   
+   });
+});
